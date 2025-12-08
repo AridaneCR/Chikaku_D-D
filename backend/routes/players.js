9d0d674bd2b9f6365a00f06bdee7761d11bf2557
aridane <aridanecabreraramirez@gmail.com>
2025-12-08 15:27:57 +0100

    Inicio

--- backend/routes/players.js
+++ backend/routes/players.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Player = require("../models/Player");
+const Player = require("../models/player");
 const multer = require("multer");
 
 // Multer en memoria (para guardar imágenes como Base64)
