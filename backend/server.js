c36462481e53d4e5f04f203a099477d5881c6049
aridane <aridanecabreraramirez@gmail.com>
2026-01-03 18:38:24 +0000

    desactivar cache server

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,11 @@ const app = express();
 // =============================================================
 // MIDDLEWARES
 // =============================================================
+app.use((req, res, next) => {
+  res.set("Cache-Control", "no-store");
+  next();
+});
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
