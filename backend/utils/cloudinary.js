bc52115c610046d5a1544679d21c34fb04c114bd
aridane <aridanecabreraramirez@gmail.com>
2026-01-03 16:35:09 +0000

    añadir forzado de nueva public_id para imagenes

--- backend/utils/cloudinary.js
+++ backend/utils/cloudinary.js
@@ -1,5 +1,7 @@
 // backend/utils/cloudinary.js
 const cloudinary = require("cloudinary").v2;
+const { v4: uuidv4 } = require("uuid");
+
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -15,6 +17,8 @@ async function uploadBuffer(buffer, folder = "dnd") {
     cloudinary.uploader.upload_stream(
       {
         folder,
+        public_id: uuidv4(), // 🔥 SIEMPRE ÚNICO
+        overwrite: true,     // 🔒 evita reutilización
         resource_type: "image",
         transformation: [
           { width: 512, height: 512, crop: "limit" },
@@ -32,6 +36,7 @@ async function uploadBuffer(buffer, folder = "dnd") {
     ).end(buffer);
   });
 }
+
 
 async function uploadImage(buffer, folder) {
   return uploadBuffer(buffer, folder);
